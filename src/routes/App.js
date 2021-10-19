1560f2dc4895033fc20c5d3d8fdfe2c687e0ddb6
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2021-10-19 02:08:48 -0500

    Activity06 detect forbidden word in RFC

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import IndexContainer from "../containers/IndexContainer";
 import LoggedInContainer from "../containers/LoggedInContainer";
+import RFCContainer from "../containers/RFCContainer";
 import WelcomeContainer from "../containers/WelcomeContainer";
 
 const App = () => {
@@ -16,6 +17,14 @@ const App = () => {
         </Route>
         <Route exact path="/loggedin">
           <LoggedInContainer />
+        </Route>
+        <Route exact path="/rfc">
+          <RFCContainer />
+        </Route>
+        <Route path="/">
+          <div className="container mt-5 text-center">
+            <h1>404 NOT found</h1>
+          </div>
         </Route>
       </Switch>
     </BrowserRouter>
